d85c8e23b0149c21528f1a73aeaa780aa3758d9d
avishekBS <a.b.sribasbanikam@gmail.com>
2021-09-20 23:08:49 +0530

    first spring boot api

---
+++ springBoot/src/main/java/io/explore/spring/boot/api/MyAppAPI.java
@@ -0,0 +1,13 @@
+package io.explore.spring.boot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppAPI.class, args);
+	}
+
+}
