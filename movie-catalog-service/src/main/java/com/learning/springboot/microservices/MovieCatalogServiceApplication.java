7c0215c6decf2048cdbe74ecdea400229697e6a4
avishekBS <a.b.sribasbanikam@gmail.com>
2021-09-30 17:47:05 +0530

    HttpComponentsClientHttpRequestFactory   for timeout

--- movie-catalog-service/src/main/java/com/learning/springboot/microservices/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/learning/springboot/microservices/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.Builder;
@@ -16,7 +17,10 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		//for time out use HttpComponentsClientHttpRequestFactory
+		HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		httpComponentsClientHttpRequestFactory.setConnectionRequestTimeout(3000);
+		return new RestTemplate(httpComponentsClientHttpRequestFactory);
 	}
 	
 	@Bean
